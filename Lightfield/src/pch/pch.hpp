7312858f30eeed594be5c72c48efb67f0cbb1137
Jan <33833587+Model2TypeE@users.noreply.github.com>
2022-01-13 17:45:06 +0100

    included directxtk

--- Lightfield/src/pch/pch.hpp
+++ Lightfield/src/pch/pch.hpp
@@ -42,6 +42,27 @@
 	// Shader Compiler
 	#pragma comment(lib, "D3DCompiler.lib")
 	#include <d3dcompiler.h>
+
+	// DirectXTK (only really need this to save textures to image files)
+	//#include "BufferHelpers.h"
+	//#include "CommonStates.h"
+	//#include "DDSTextureLoader.h"
+	//#include "DirectXHelpers.h"
+	//#include "Effects.h"
+	//#include "GamePad.h"
+	//#include "GeometricPrimitive.h"
+	//#include "GraphicsMemory.h"
+	//#include "Keyboard.h"
+	//#include "Model.h"
+	//#include "Mouse.h"
+	//#include "PostProcess.h"
+	//#include "PrimitiveBatch.h"
+	#include "ScreenGrab.h"
+	//#include "SimpleMath.h"
+	//#include "SpriteBatch.h"
+	//#include "SpriteFont.h"
+	//#include "VertexTypes.h"
+	//#include "WICTextureLoader.h"
 #endif
 
 // utils
